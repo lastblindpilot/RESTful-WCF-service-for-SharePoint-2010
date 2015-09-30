88056140cedc17ed1dac6b265b25101e8718b31f
lastblindpilot <fraxb7@gmail.com>
2015-09-30 13:47:01 +0300

    first

---
+++ IOrderService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data;
+
+namespace SPWcfExample
+{
+    [ServiceContract(Namespace = "http:www.Test.com/SPWcfExample/OrderService/2012/06", Name = "OrderService")]
+    public interface IOrderService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", BodyStyle = WebMessageBodyStyle.Wrapped,
+            RequestFormat = WebMessageFormat.Xml, 
+            UriTemplate = "/GetOrder/{customerName}")]
+            Order GetOrder(string CustomerName);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", 
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            RequestFormat = WebMessageFormat.Xml, 
+            UriTemplate = "/PlaceOrder")]
+        int PlaceOrder(Order order);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", 
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            RequestFormat = WebMessageFormat.Xml, 
+            UriTemplate = "/Test")]
+        string Test();
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", 
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            RequestFormat = WebMessageFormat.Xml,
+            UriTemplate = "GetData/all")]
+        List<Employee> GetAllEmployeesMethod();
+    }
+
+    [DataContract(Namespace = "http:www.Test.com/SPWcfExample/OrderService/2012/06", Name = "Order")]
+    public class Order
+    {
+
+        [DataMember]
+        public int OrderNumber { get; set; }
+        [DataMember]
+        public int ProductNumber { get; set; }
+        [DataMember]
+        public int Quantity { get; set; }
+        [DataMember]
+        public string CustomerName { get; set; }
+    }
+
+    [DataContract]
+    public class Employee
+    {
+        [DataMember]
+        public string firstname { get; set; }
+        [DataMember]
+        public string lastname { get; set; }
+        [DataMember]
+        public decimal salary { get; set; }
+        public Employee(string first, string last, decimal sal)
+        {
+            firstname = first;
+            lastname = last;
+            salary = sal;
+        }
+    }
+}
---
+++ OrderService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+
+namespace SPWcfExample
+{
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class OrderService : IOrderService
+    {
+        public Order GetOrder(string customerName)
+        {
+            Order order = new Order
+            {
+                CustomerName = "Metro",
+                OrderNumber = 101,
+                ProductNumber = 100,
+                Quantity = 5
+            };
+
+            return order;
+        }
+
+        public int PlaceOrder(Order order)
+        {
+            return 101;
+        }
+
+        public string Test()
+        {
+            return "Hello World";
+        }
+
+        public List<Employee> GetAllEmployeesMethod()
+        {
+            List<Employee> mylist = new List<Employee>();
+
+            using (SqlConnection conn = new SqlConnection("Data Source=server;Initial Catalog=Inventory;Integrated Security=SSPI;"))
+            {
+                conn.Open();
+
+                string cmdStr = String.Format("Select firstname,lastname,salary from dataWcfTest");
+                SqlCommand cmd = new SqlCommand(cmdStr, conn);
+                SqlDataReader rd = cmd.ExecuteReader();
+
+                if (rd.HasRows)
+                {
+                    while (rd.Read())
+                        mylist.Add(new Employee(rd.GetString(0), rd.GetString(1), rd.GetDecimal(2)));
+                }
+                conn.Close();
+            }
+
+            return mylist;
+        }
+    }
+}
